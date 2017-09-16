5018ba0ef3ef825cddd47c4cb35e3feee252b0b4
Dongho Gang <gongdongho12@gmail.com>
2017-09-17 05:37:00 +0900

    bug fix board rows.

--- public/javascripts/board.js
+++ public/javascripts/board.js
@@ -1,6 +1,6 @@
 //테이블 실행
 $(document).ready(function() {
-    $(".card tr").each(function() {
+    $(".board tr").each(function() {
         var context = $(this);
         context.find("td").slice(0, 4).click(function() {
             location.href = "/board/" + $(this).parents().children().eq(0).text().trim();
